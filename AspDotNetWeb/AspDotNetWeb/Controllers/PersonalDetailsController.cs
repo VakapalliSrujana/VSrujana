05ef891653f1ac2bbae079c4aa72622baa25e07a
Vakapalli Srujana <svakapalli@tollplus.com>
2019-01-23 16:45:04 +0530

    Added Mvc PersonalDetailsProject

---
+++ AspDotNetWeb/AspDotNetWeb/Controllers/PersonalDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspDotNetWeb.Models;
+
+namespace AspDotNetWeb.Controllers
+{
+    public class PersonalDetailsController : Controller
+    {
+        private AspDotNetWebContext db = new AspDotNetWebContext();
+
+        // GET: PersonalDetails
+        public ActionResult Index()
+        {
+            return View(db.PersonalDetails.ToList());
+        }
+
+        // GET: PersonalDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalDetail personalDetail = db.PersonalDetails.Find(id);
+            if (personalDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalDetail);
+        }
+
+        // GET: PersonalDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonalDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AutoId,FirstName,LastName,Age,DateOfBirth")] PersonalDetail personalDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PersonalDetails.Add(personalDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personalDetail);
+        }
+
+        // GET: PersonalDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalDetail personalDetail = db.PersonalDetails.Find(id);
+            if (personalDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalDetail);
+        }
+
+        // POST: PersonalDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AutoId,FirstName,LastName,Age,DateOfBirth")] PersonalDetail personalDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personalDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personalDetail);
+        }
+
+        // GET: PersonalDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalDetail personalDetail = db.PersonalDetails.Find(id);
+            if (personalDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalDetail);
+        }
+
+        // POST: PersonalDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PersonalDetail personalDetail = db.PersonalDetails.Find(id);
+            db.PersonalDetails.Remove(personalDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AspDotNetWeb/AspDotNetWeb/Models/PersonalDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Linq;
+using System.Web;
+
+namespace AspDotNetWeb.Models
+{
+    public class PersonalDetail
+    {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int AutoId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+        [DataType(DataType.Date)]
+        public string DateOfBirth { get; set; }
+    }
+}
